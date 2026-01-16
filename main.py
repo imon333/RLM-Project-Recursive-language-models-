d92ad65c402321eaef4326bd9d9222b4c1b3228f
imon333 <imonhosen99@gmail.com>
2026-01-16 11:07:44 +0100

    the AI provide correct answer and can find them but the problem is that some times it does not provide good code and it's does not work and give - task could not be completed in time-

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from termcolor import colored
 massive_context = ("This is a filler sentence. " * 1000) + \
                   " SECRET_PROJECT_ID: 'ORION-99' " + \
                   ("This is more noise. " * 1000) +\
-                ("Germany is aswesome and very good . " * 1000) + \
+                ("Germany is awesome and very good . " * 1000) + \
                   " SECRET_MOVIE: 'FURY " + \
                   ("This is more noise. " * 1000)
 
@@ -20,12 +20,12 @@ def main():
     agent = RLMAgent(massive_context)
 
     # The question
-    query = "Search through the context variable and find the SECRET_PROJECT_ID. and Secret Movie"
+    query = "Search through the context variable and find the SECRET_PROJECT_ID."
 
     answer = agent.solve(query)
 
     print("\n" + "=" * 50)
-    print(colored(f"THE FINAL ANSWER: {answer}", "green", attrs=["bold"]))
+    print(colored(f"THE FINAL ANSWER: {answer}", "blue", attrs=["bold"]))
     print("=" * 50)
 
 
