d06ba4f5e74781543a3f3eefb9702862930ffb37
imon333 <imonhosen99@gmail.com>
2026-04-22 15:03:25 +0200

    remove some context from there

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ massive_context = ("This is a filler sentence. " * 1000) + \
                   " SECRET_MOVIE or secret movie i liked: 'Jobs' " + \
                   ("USA is has dectator leadership" * 2000) + \
                   " imon's likes: 'biriyani' " + \
-                  " SECRET_MOVIE or secret movie i liked: 'Jobs' " + \
-                  ("USA is has dectator leadership" * 2000) + \
-                  " imon's likes: 'biriyani' "
+                  " SECRET_MOVIE or secret movie i liked: 'Jobs' "
 
 
 
